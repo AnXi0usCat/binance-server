8fa26fdc8558b860379ab3e72762553123c292ca
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-04-08 21:50:38 +0100

    initiial commit for binance client

---
+++ binance-client/src/lib.rs
@@ -0,0 +1,50 @@
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, WebSocketStream, tungstenite::protocol::Message, MaybeTlsStream};
+use serde_json::Value;
+use futures_util::{StreamExt, SinkExt};
+use std::error::Error;
+
+pub struct BinanceWebSocket {
+    ws_stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
+}
+
+impl BinanceWebSocket {
+    pub async fn connect(url: &str) -> Result<Self, Box<dyn Error>> {
+        let (ws_stream, _) = connect_async(url).await?;
+        Ok(Self { ws_stream })
+    }
+
+    pub async fn subscribe(&mut self, stream: &str) -> Result<(), Box<dyn Error>> {
+        let subscribe_msg = format!(r#"{{"method": "SUBSCRIBE", "params": ["{}"], "id": 1}}"#, stream);
+        self.ws_stream.send(Message::Text(subscribe_msg)).await?;
+        Ok(())
+    }
+
+    pub async fn unsubscribe(&mut self, stream: &str) -> Result<(), Box<dyn Error>> {
+        let unsubscribe_msg = format!(r#"{{"method": "UNSUBSCRIBE", "params": ["{}"], "id": 1}}"#, stream);
+        self.ws_stream.send(Message::Text(unsubscribe_msg)).await?;
+        Ok(())
+    }
+
+    pub async fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        while let Some(msg) = self.ws_stream.next().await {
+            let msg = msg?;
+
+            match msg {
+                Message::Text(text) => {
+                    let value: Value = serde_json::from_str(&text)?;
+                    println!("{}", value);
+                }
+                Message::Ping(ping) => {
+                    println!("Received Ping: {:?}", ping);
+                    self.ws_stream.send(Message::Pong(ping)).await?;
+                    println!("Sent Pong");
+                }
+                _ => (),
+            }
+        }
+
+        Ok(())
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate binance_client as client;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+    let url = "wss://stream.binance.com:9443/ws";
+    let mut binance_ws = client::BinanceWebSocket::connect(url).await?;
+
+    binance_ws.subscribe("btcusdt@bookTicker").await?;
+
+    binance_ws.run().await?;
+
+    Ok(())
+}
