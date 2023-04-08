d0af7d7740cd48d2f27188e6d0e537f2264d3464
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-04-08 23:39:08 +0100

    add cli for the app -> doesnt work

--- binance-client/src/lib.rs
+++ binance-client/src/lib.rs
@@ -9,29 +9,29 @@ pub struct BinanceWebSocket {
 }
 
 impl BinanceWebSocket {
-    pub async fn connect(url: &str) -> Result<Self, Box<dyn Error>> {
+    pub async fn connect(url: &str) -> Result<Self, Box<dyn Error + Send + Sync>> {
         let (ws_stream, _) = connect_async(url).await?;
         Ok(Self { ws_stream })
     }
 
-    pub async fn combined_stream(&mut self, combined: bool) -> Result<(), Box<dyn Error>> {
+    pub async fn combined_stream(&mut self, combined: bool) -> Result<(), Box<dyn Error + Send + Sync>> {
         let property_msg = format!(r#"{{"method": "SET_PROPERTY", "params": ["combined", {}], "id": 1}}"#, combined);
         self.ws_stream.send(Message::Text(property_msg)).await?;
         Ok(())
     }
-    pub async fn subscribe(&mut self, stream: &str) -> Result<(), Box<dyn Error>> {
+    pub async fn subscribe(&mut self, stream: &str) -> Result<(), Box<dyn Error + Send + Sync>> {
         let subscribe_msg = format!(r#"{{"method": "SUBSCRIBE", "params": ["{}"], "id": 1}}"#, stream);
         self.ws_stream.send(Message::Text(subscribe_msg)).await?;
         Ok(())
     }
 
-    pub async fn unsubscribe(&mut self, stream: &str) -> Result<(), Box<dyn Error>> {
+    pub async fn unsubscribe(&mut self, stream: &str) -> Result<(), Box<dyn Error + Send + Sync>> {
         let unsubscribe_msg = format!(r#"{{"method": "UNSUBSCRIBE", "params": ["{}"], "id": 1}}"#, stream);
         self.ws_stream.send(Message::Text(unsubscribe_msg)).await?;
         Ok(())
     }
 
-    pub async fn run(&mut self) -> Result<(), Box<dyn Error>> {
+    pub async fn run(&mut self) -> Result<(), Box<dyn Error + Send + Sync>> {
         while let Some(msg) = self.ws_stream.next().await {
             let msg = msg?;
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,95 @@
 extern crate binance_client as client;
+use clap::{App, Arg, SubCommand};
 use std::error::Error;
+use tokio::io::{self, AsyncBufReadExt, BufReader};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let url = "wss://stream.binance.com:9443/ws";
     let mut binance_ws = client::BinanceWebSocket::connect(url).await?;
+    let ws_handle = tokio::spawn(async move { binance_ws.run().await });
 
-    binance_ws.combined_stream(true).await?;
-    binance_ws.subscribe("btcusdt@trade").await?;
-    binance_ws.subscribe("btcusdt@bookTicker").await?;
+    let cli = App::new("Binance WebSocket CLI")
+        .version("1.0")
+        .author("AnXi0usCat <mikhail.fjodorov@gmail.com>")
+        .about("Interacts with Binance WebSocket API")
+        .subcommand(
+            SubCommand::with_name("subscribe")
+                .about("Subscribes to a stream")
+                .arg(
+                    Arg::with_name("STREAM")
+                        .help("The stream to subscribe to")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("unsubscribe")
+                .about("Unsubscribes from a stream")
+                .arg(
+                    Arg::with_name("STREAM")
+                        .help("The stream to unsubscribe from")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("combined")
+                .about("Creates combined stream")
+                .arg(
+                    Arg::with_name("STREAM")
+                        .help("Use combined stream for subscrptions")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("exit").about("exit application").arg(
+                Arg::with_name("STREAM")
+                    .help("Use combined stream for subscrptions")
+                    .required(true)
+                    .index(1),
+            ),
+        );
 
-    binance_ws.run().await?;
+    let mut reader = BufReader::new(io::stdin()).lines();
 
+    while let Some(line) = reader.next_line().await? {
+        let parts: Vec<&str> = line.trim().split_whitespace().collect();
+
+        if let Ok(matches) = cli.clone().try_get_matches_from(parts) {
+            match matches.subcommand() {
+                ("subscribe", sub_m) => {
+                    let stream = sub_m.value_of("STREAM").unwrap();
+                    binance_ws.subscribe(stream).await?;
+                    println!("Subscribed to stream: {}", stream);
+                }
+                ("unsubscribe", Some(sub_m)) => {
+                    let stream = sub_m.value_of("STREAM").unwrap();
+                    binance_ws.unsubscribe(stream).await?;
+                    println!("Unsubscribed from stream: {}", stream);
+                }
+                ("combined", Some(sub_m)) => {
+                    let stream = sub_m.value_of("STREAM").unwrap();
+                    binance_ws.combined_stream(stream).await?;
+                    println!("Use combined stream: {}", stream);
+                }
+
+                ("exit", Some(sub_m)) => {
+                    let stream = sub_m.value_of("STREAM").unwrap();
+                    println!("shitting down application: {}", stream);
+                    break;
+                }
+                _ => {}
+            }
+        } else {
+            println!("Invalid command or arguments. Supported commands are:");
+            println!("  subscribe <stream>");
+            println!("  unsubscribe <stream>");
+            println!("  exit");
+        }
+    }
+
+    ws_handle.await??;
     Ok(())
 }
