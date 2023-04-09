d9a11b7a182481bf5ffc83cab55db58e2b3ae0f4
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-04-09 11:12:42 +0100

    add helper function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
-extern crate binance_client as client;
+use binance_client::{BinanceWebSocket, Command};
 use clap::{App, Arg, SubCommand};
 use std::error::Error;
 use tokio::io::{self, AsyncBufReadExt, BufReader};
 
+fn parse_command(input: &str, cli: &App) -> Option<Command> {
+    let parts: Vec<&str> = input.trim().split_whitespace().collect();
+    let matches = cli.clone().try_get_matches_from(parts).ok()?;
+
+    if let Some(sub_m) = matches.subcommand_matches("subscribe") {
+        let stream = sub_m.value_of("STREAM").unwrap().to_string();
+        Some(Command::Subscribe(stream))
+    } else if let Some(sub_m) = matches.subcommand_matches("unsubscribe") {
+        let stream = sub_m.value_of("STREAM").unwrap().to_string();
+        Some(Command::Unsubscribe(stream))
+    } else if let Some(sub_m) = matches.subcommand_matches("combined") {
+        let stream = sub_m.value_of("STREAM").unwrap().parse().unwrap();
+        Some(Command::CombinedStream(stream))
+    } else if matches.subcommand_matches("exit").is_some() {
+        Some(Command::Exit)
+    } else {
+        None
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let url = "wss://stream.binance.com:9443/ws";
-    let mut binance_ws = client::BinanceWebSocket::connect(url).await?;
-    let ws_handle = tokio::spawn(async move { binance_ws.run().await });
+    let mut binance_ws = BinanceWebSocket::connect(url).await?;
 
     let cli = App::new("Binance WebSocket CLI")
         .version("1.0")
@@ -55,41 +74,32 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let mut reader = BufReader::new(io::stdin()).lines();
 
     while let Some(line) = reader.next_line().await? {
-        let parts: Vec<&str> = line.trim().split_whitespace().collect();
-
-        if let Ok(matches) = cli.clone().try_get_matches_from(parts) {
-            match matches.subcommand() {
-                ("subscribe", sub_m) => {
-                    let stream = sub_m.value_of("STREAM").unwrap();
-                    binance_ws.subscribe(stream).await?;
-                    println!("Subscribed to stream: {}", stream);
-                }
-                ("unsubscribe", Some(sub_m)) => {
-                    let stream = sub_m.value_of("STREAM").unwrap();
-                    binance_ws.unsubscribe(stream).await?;
-                    println!("Unsubscribed from stream: {}", stream);
-                }
-                ("combined", Some(sub_m)) => {
-                    let stream = sub_m.value_of("STREAM").unwrap();
-                    binance_ws.combined_stream(stream).await?;
-                    println!("Use combined stream: {}", stream);
-                }
-
-                ("exit", Some(sub_m)) => {
-                    let stream = sub_m.value_of("STREAM").unwrap();
-                    println!("shitting down application: {}", stream);
-                    break;
-                }
-                _ => {}
+        match parse_command(&line, &cli) {
+            Some(Command::Subscribe(stream)) => {
+                binance_ws.subscribe(&stream).await?;
+                println!("Subscribed to stream: {}", stream);
             }
-        } else {
-            println!("Invalid command or arguments. Supported commands are:");
-            println!("  subscribe <stream>");
-            println!("  unsubscribe <stream>");
-            println!("  exit");
+            Some(Command::Unsubscribe(stream)) => {
+                binance_ws.unsubscribe(&stream).await?;
+                println!("Unsubscribed from stream: {}", stream);
+            }
+            Some(Command::CombinedStream(stream)) => {
+                binance_ws.combined_stream(stream).await?;
+                println!("Creating a combined stream: {}", stream);
+            }
+            Some(Command::Exit) => {
+                break;
+            }
+            None => {
+                println!("Invalid command or arguments. Supported commands are:");
+                println!("  subscribe <stream>");
+                println!("  unsubscribe <stream>");
+                println!("  exit");
+            }
         }
     }
 
+    let ws_handle = tokio::spawn(async move { binance_ws.run().await });
     ws_handle.await??;
     Ok(())
 }
