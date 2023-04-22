78d3ac055ff1c0d56ae95a9770053d78a261c3d7
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-04-22 22:36:49 +0100

    add associated functions to generate signature and get timestamp

--- client/src/rest.rs
+++ client/src/rest.rs
@@ -1,4 +1,41 @@
-
+use reqwest::Client;
+use hmac::{Hmac, Mac};
+use sha2::Sha256;
 
 pub struct BinanceRest {
-} 
+    client: Client,
+    api_key: String,
+    secret_key: String,
+    base_url: String,
+}
+
+impl BinanceRest {
+
+    pub fn new(api_key: String, secret_key: String) -> Self {
+        BinanceRest { 
+            client: Client::new(), 
+            api_key, 
+            secret_key, 
+            base_url: String::from("https://api.binance.com/api") 
+        }
+    }
+
+    fn get_timestamp() -> String {
+        let start = std::time::SystemTime::now();
+        let since_epoch = start
+            .duration_since(std::time::UNIX_EPOCH).expect("Time went backwards");
+        since_epoch.as_millis().to_string()
+    }
+
+    fn create_signature(&self, timestamp: &str) -> String {
+        let mut mac = Hmac::<Sha256>::new_from_slice(self.secret_key.as_bytes())
+            .expect("HMAC can take key of any size");
+        let query = format!("timestamp={}", timestamp);
+        mac.update(query.as_bytes());
+
+        let result = mac.finalize();
+        let signature = hex::encode(result.into_bytes());
+
+        signature
+    }
+}
