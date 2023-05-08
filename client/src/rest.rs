c9cabe59c034b515a83dc5945acc7b2880629e59
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-05-08 17:52:36 +0100

    create new limit order `test`

--- client/src/rest.rs
+++ client/src/rest.rs
@@ -1,7 +1,19 @@
 use reqwest::{ Client, Response };
 use hmac::{Hmac, Mac};
 use sha2::Sha256;
-use serde::Serialize;
+use serde::{ Serialize, Deserialize };
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LimitOrder {
+    symbol: String,
+    side: String,
+    r#type: String,
+    time_in_force: String,
+    quantity: String,
+    price: String,
+    timestamp: String,
+    signature: String
+}
 
 pub struct BinanceRest {
     client: Client,
@@ -21,7 +33,7 @@ impl BinanceRest {
         }
     }
 
-    fn get_timestamp() -> String {
+    fn get_timestamp(&self) -> String {
         let start = std::time::SystemTime::now();
         let since_epoch = start
             .duration_since(std::time::UNIX_EPOCH).expect("Time went backwards");
@@ -40,7 +52,7 @@ impl BinanceRest {
         signature
     }
 
-    async fn post<T: Serialize + ?Sized >(&self, json_body: &T, query: &str) -> Result<Response, reqwest::Error> {
+    async fn post<T: Serialize + ?Sized>(&self, json_body: &T, query: &str) -> Result<Response, reqwest::Error> {
         let url = format!("{}/{}", &self.base_url, query);
         let response = self.client.post(&url)
             .header("X-MBX-APIKEY", &self.api_key)
@@ -69,5 +81,34 @@ impl BinanceRest {
             .await;
 
         response
+    }
+
+    pub async fn test_limit_order(&self, symbol: String, side: String, quantity: String, price: String) {
+        let url = "api/v3/order/test";
+        let timestamp = self.get_timestamp();
+
+        let limit_order = LimitOrder { 
+            symbol,
+            side,
+            r#type: String::from("LIMIT"),
+            time_in_force: String::from("GTC"),
+            quantity,
+            price,
+            timestamp: timestamp.clone(),
+            signature: self.create_signature(&timestamp)
+        };
+
+        let response = self.post(&limit_order, url).await;
+        print!("received response {:?}", response);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_create_new_limit_order() {
+
     }
 }
